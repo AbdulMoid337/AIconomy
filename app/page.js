2ed1dfaf1207b0d5283b9dd67b951bf46b000ff0
Abdul Moid <abdulmoid337@gmail.com>
2024-12-24 18:28:03 +0530

    first

--- app/page.js
+++ app/page.js
@@ -1,7 +1,4 @@
-import Image from "next/image";
-
+import { Button } from "@/components/ui/button"
 export default function Home() {
-  return <div> hello
-    <h1>dd</h1>
-  </div>
+  return   <Button variant="destructive">hrth</Button>
 }
